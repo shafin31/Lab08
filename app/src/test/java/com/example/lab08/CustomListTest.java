d52424e21d3bc5fef82d5cde24546ce64accb9e0
Your Name <shafin3@ualberta.ca>
2026-03-06 16:13:06 -0700

    count city

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -12,4 +12,12 @@ public class CustomListTest {
         assertTrue(list.hasCity(calgary));
     }
 
+    @Test
+    public void testCountCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.deleteCity(calgary));
+    }
+
 }
